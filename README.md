d2008d35b5df913ec0d7cea9f33f80a22e95bcf2
maksim niveykin <maksniv@gmail.com>
2024-08-11 18:49:53 +0300

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# test-task-shopping-cart
-
-```bash
-# serve with hot reload at localhost:3000
-$ npm install
-$ npm run dev
-```
--- plugins/swiper.client.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
-
-Vue.prototype.$swiper = Swiper;
